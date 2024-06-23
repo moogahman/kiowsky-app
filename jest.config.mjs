9bc84c1d40a2ea629ad0f53be546132975766c76
Jedd de Gouveia <jedd@moogah.net>
2024-06-24 00:50:54 +1000

    Update jest config file to work with CI workflow

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,4 +2,9 @@ export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['dist'],
+    transform: { '\\.[jt]sx?$': ['ts-jest', { useESM: true }] },
+    moduleNameMapper: {
+        '(.+)\\.js': '$1',
+    },
+    extensionsToTreatAsEsm: ['.ts'],
 };
