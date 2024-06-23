0f8f4325119f5698cd3dc158858e579ee5727851
Jedd de Gouveia <jedd@moogah.net>
2024-06-24 00:47:56 +1000

    Simplify jest config file

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,19 +1,5 @@
 export default {
-    preset: 'ts-jest/presets/default-esm', // Use this preset for ESM support
+    preset: 'ts-jest',
     testEnvironment: 'node',
-    extensionsToTreatAsEsm: ['.ts'], // Treat .ts files as ESM
-    globals: {
-        'ts-jest': {
-            useESM: true,
-        },
-    },
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1', // Resolve .js extensions to .ts files
-    },
-    transform: {
-        '^.+\\.ts$': 'ts-jest', // Transform TypeScript files
-    },
-    transformIgnorePatterns: [
-        'node_modules/(?!firebase)', // Add modules you want to be transformed by Jest here
-    ],
+    testPathIgnorePatterns: ['dist'],
 };
