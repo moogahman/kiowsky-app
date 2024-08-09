620c1471b9c91b63198a5da5bbf5ff0b2f776d4d
HannesEnnemann <hannesthe1@proton.me>
2024-08-09 14:28:49 +1000

    item card

---
+++ src/components/items/item.tsx
@@ -0,0 +1,26 @@
+import './item.css';
+
+interface ItemProps {
+    price: number;
+    title: string;
+    link: string;
+    image: string;
+}
+
+export function Item({ price, title, link, image }: ItemProps) {
+    return (
+        <div className="container-item">
+            <div className="image-container">
+                <img className="item-image" alt="Item image" src={image} />
+            </div>
+            <div className="text-container">
+                <a href={link} className="item-title">
+                    {title}
+                </a>
+                <h4 className="item-price">
+                    AUD$ <span className="item-price-span">{price}</span>
+                </h4>
+            </div>
+        </div>
+    );
+}
