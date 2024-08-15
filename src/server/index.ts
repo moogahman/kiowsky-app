694505b7dc099ac0701b1aa2f04f6bc78a94b35f
Jedd <jedd@moogah.net>
2024-08-15 19:45:15 +1000

    Update index.ts to start vite express server when on development

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import * as functions from 'firebase-functions';
 import helmet from 'helmet';
+import ViteExpress from 'vite-express';
 import { getFileURL } from './firebase/getFileURL.js';
 import { getItems } from './firebase/getItems.js';
 import { verifyCode } from './firebase/verifyCode.js';
@@ -63,5 +64,12 @@ app.post('/api/verify-code', async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+if (process.env.NODE_ENV === 'development') {
+    ViteExpress.config({ mode: 'development' });
+    ViteExpress.listen(app, 3000, () =>
+        console.log('Server is listening on port 3000...')
+    );
+}
 
 export { api, app };
