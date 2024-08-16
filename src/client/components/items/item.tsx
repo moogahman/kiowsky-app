cfcbd065d23e20fc4b0d708ce0a69bc1cd3f6f7f
HannesEnnemann <hannesthe1@proton.me>
2024-08-16 17:05:30 +1000

    no image scenario placeholder

--- src/client/components/items/item.tsx
+++ src/client/components/items/item.tsx
@@ -1,7 +1,11 @@
 import type { MenuItemDisplayProps } from '../../types';
 import './item.css';
 
+const defaultImage = './img/noImage.png';
+
 export function Item({ price, title, link, image }: MenuItemDisplayProps) {
+    const displayImage = image || defaultImage;
+
     return (
         <div
             className="container-item"
@@ -9,7 +13,11 @@ export function Item({ price, title, link, image }: MenuItemDisplayProps) {
                 window.location.href = link;
             }}>
             <div className="image-container">
-                <img className="item-image" alt="Item image" src={image} />
+                <img
+                    className="item-image"
+                    alt="Item image"
+                    src={displayImage}
+                />
             </div>
             <div className="text-container">
                 <a className="item-title">{title}</a>
