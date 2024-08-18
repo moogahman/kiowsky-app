4ae3123e073ee98aa0a55c7f7d6ea5d27583ef13
Jedd <jedd@moogah.net>
2024-08-18 23:35:07 +1000

    Only allow certain websites to access the api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,17 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            'http://localhost:3000',
+            'http://localhost:5173',
+            'https://kiowsky.web.app/',
+            'https://kiowsky.firebaseapp.com/',
+            /^https:\/\/kiowsky--.*\.web\.app$/,
+        ],
+    })
+);
 app.use(express.json());
 
 // Routes
