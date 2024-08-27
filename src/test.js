a834b8b79300e35b7c34806bf2bd968cf639cbd4
Jedd <jedd@moogah.net>
2024-08-27 20:35:28 +1000

    idk what i did

--- src/test.js
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-
-console.log(import.meta.env.DEV);
-console.log(import.meta.env.VITE_API_BASE_URL);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import react from '@vitejs/plugin-react';
-import { config } from 'dotenv';
-import { defineConfig } from 'vite';
+import 'dotenv/config';
 
-config();
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import env from 'vite-plugin-env-compatible';
+import vitetsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), vitetsConfigPaths(), env()],
+    define: {
+        'process.env': process.env,
+    },
 });
