1eed1ec759fda4115790b8c3ff1c2a7738f697e7
Jedd <jedd@moogah.net>
2024-08-30 09:42:18 +1000

    See whos right

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import 'dotenv/config';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import env from 'vite-plugin-env-compatible';
-import vitetsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), vitetsConfigPaths(), env()],
-    define: {
-        'process.env': process.env,
-    },
+    plugins: [
+        react(),
+        //	vitetsConfigPaths(), env()
+    ],
+    // define: {
+    //     'process.env': process.env,
+    // },
 });
